021e8118d6169e64d01e7b86a4916b115fe504e6
Frostquill <ruiz82494@gmail.com>
2021-07-01 13:05:19 -0700

    added voter routes

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const db = require('./db/connection');
-const inputCheck = require('./utils/inputCheck');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
